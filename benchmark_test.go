3ff312834a6f49d71a105d0bc9c4b3ff7a1604bc
Rafał Lorenz <vardius@gmail.com>
2020-01-30 23:55:22 +1100

    Test write in parallel

--- benchmark_test.go
+++ benchmark_test.go
@@ -1,19 +1,18 @@
 package lrucache
 
 import (
-	"strconv"
 	"testing"
 )
 
 func BenchmarkRead(b *testing.B) {
 	c := New(10)
 
-	c.Set("1", 1)
+	c.Set("value", 1)
 
 	b.ResetTimer()
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			if c.Get("1") == nil {
+			if c.Get("value") == nil {
 				b.Fail()
 			}
 		}
@@ -24,8 +23,9 @@ func BenchmarkWrite(b *testing.B) {
 	c := New(b.N)
 
 	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		c.Set(strconv.Itoa(i), i)
-	}
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			c.Set("value", 1)
+		}
+	})
 }
